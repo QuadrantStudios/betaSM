5078175375fd0f63b7b7f2a964052b6c23440302
Phillip <you@example.com>
2017-06-25 18:29:47 +1000

    Completed FB integration

---
+++ Social Media/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  Social Media
+//
+//  Created by Phillip on 25/6/17.
+//  Copyright © 2017 Phillip Ilievski. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
